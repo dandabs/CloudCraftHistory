baf0e73c5e616845c446f4d9ca0d36c7f35c9cb7
Daniel Adams <danstanswinxp@gmail.com>
2020-12-02 17:12:34 +0000

    Nginx reverse-proxy

--- CloudCraftHistory/Startup.cs
+++ CloudCraftHistory/Startup.cs
@@ -9,6 +9,8 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
+using Microsoft.AspNetCore.HttpOverrides;
+
 namespace CloudCraftHistory
 {
     public class Startup
@@ -35,6 +37,12 @@ namespace CloudCraftHistory
             }
             else
             {
+
+app.UseForwardedHeaders(new ForwardedHeadersOptions
+{
+    ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto
+});
+
                 app.UseExceptionHandler("/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
