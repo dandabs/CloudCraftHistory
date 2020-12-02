a0baba9314066cf7595e96a901a3b7a45ec6a581
Daniel Adams <danstanswinxp@gmail.com>
2020-12-02 18:08:19 +0000

    Layout and User model

--- CloudCraftHistory/wwwroot/js/site.js
+++ CloudCraftHistory/wwwroot/js/site.js
@@ -2,3 +2,13 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your Javascript code.
+
+// Add slideDown animation to dropdown
+$('.dropdown').on('show.bs.dropdown', function(e){
+    $(this).find('.dropdown-menu').first().stop(true, true).slideDown();
+  });
+  
+  // Add slideUp animation to dropdown
+  $('.dropdown').on('hide.bs.dropdown', function(e){
+    $(this).find('.dropdown-menu').first().stop(true, true).slideUp();
+  });
